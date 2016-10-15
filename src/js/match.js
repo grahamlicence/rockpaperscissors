81970ba925d7e6ab28c845e256bf4e62c16f7245
Graham Licence <git@grahamlicence.co.uk>
2016-10-15 14:25:43 +0100

    Match passing tests

--- src/js/match.js
+++ src/js/match.js
@@ -1,7 +1,40 @@
+import gamePlay from './gamePlay';
+
 /**
  * Handles match and game scores
  */
-const match = function() {
+const match = {
+    score: {player1: 0, player2: 0},
+
+    round: 1,
+
+    play: function(options) {
+        const outcome = gamePlay(options);
+
+        if (outcome === 'player1') {
+            this.score.player1++;
+        }
+        
+        if (outcome === 'player2') {
+            this.score.player2++;
+        }
+        this.round++;
+
+        return outcome;
+    },
+
+    getScore: function() {
+        return this.score;
+    },
+
+    getRound: function() {
+        return this.round;
+    },
+
+    reset: function() {
+        this.score = {player1: 0, player2: 0};
+        this.round = 1;
+    }
 }
 
 export default match;
